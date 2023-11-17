a59284df208743579764cec3c76d6c96548c837d
Chris <chris@dynacolor.com.tw>
2023-11-17 14:02:08 +0800

    feat: add toogle menu sample

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  const pages = [{ url: "/01-toggle-menu", title: "Toggle Menu" }];
+</script>
+
+<div class="container">
+  <div class="wrapper">
+    <h1>Svelte Practice</h1>
+    <ul>
+      {#each pages as page}
+        <li>
+          <a href={page.url}>{page.title}</a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
+
+<style>
+  .container {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    background-color: #0766ad;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  .wrapper {
+    width: 1024px;
+    margin: 0 auto;
+  }
+  h1 {
+    position: relative;
+    font-size: 30px;
+    padding: 20px 0;
+    margin-bottom: 4px;
+  }
+
+  ul {
+    list-style: decimal;
+    margin: 0;
+    padding: 0 0 0 16px;
+  }
+  li + li {
+    margin-top: 8px;
+  }
+  a {
+    font-size: 20px;
+    color: white;
+  }
+  a:hover {
+    text-decoration: none;
+  }
+</style>
---
+++ src/routes/01-toggle-menu/+page.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+  import buttonImg from "./img/button.png";
+  import { slide } from "svelte/transition";
+  import { sineInOut } from "svelte/easing";
+
+  let showMenu = false;
+</script>
+
+<div class="container">
+  <header>
+    <h1>Toggle Menu</h1>
+  </header>
+  <nav>
+    <button
+      on:click={() => {
+        showMenu = !showMenu;
+      }}><img src={buttonImg} width="20" height="17" alt="Button" /></button
+    >
+    {#if showMenu}
+      <ul transition:slide={{ duration: 200, easing: sineInOut }}>
+        <li><a href="#1">Menu1</a></li>
+        <li><a href="#2">Menu2</a></li>
+        <li><a href="#3">Menu3</a></li>
+        <li><a href="#4">Menu4</a></li>
+      </ul>
+    {/if}
+  </nav>
+  <main>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </main>
+</div>
+
+<style>
+  .container {
+    font-family: Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    color: #333;
+  }
+
+  header h1 {
+    padding: 20px 25px;
+    color: #fff;
+    background: #ea4875;
+    font-weight: bold;
+  }
+
+  button {
+    position: absolute;
+    top: 17px;
+    right: 25px;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  ul {
+    list-style-type: none;
+    position: absolute;
+    width: 100%;
+  }
+
+  li a {
+    text-decoration: none;
+    font-weight: bold;
+    display: block;
+    padding: 17px 25px;
+    color: #fff;
+    background: #b94b6a;
+    border-bottom: 1px solid #a24861;
+  }
+
+  li:last-child a {
+    border-bottom: none;
+  }
+
+  main {
+    display: block;
+    padding: 40px 20px 20px;
+    color: #333;
+  }
+
+  p {
+    margin-bottom: 50px;
+    line-height: 2em;
+  }
+</style>
